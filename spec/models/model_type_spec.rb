c4edba1b086afe3d6ef1157b4ce1f487262d1b8f
Anyelly Marrye Luvizott <any.luvizott@gmail.com>
2022-10-30 13:56:48 -0300

    Teste Unitários Model Type

--- spec/models/model_type_spec.rb
+++ spec/models/model_type_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe ModelType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    context 'presence' do
+      it 'o nome deve ser obrigatório' do
+        model_type = ModelType.new(name: nil)
+        model_type.valid?
+        expect(model_type.errors.include?(:name)).to be true
+      end
+
+      it 'a distância inicial deve ser obrigatório' do
+        model_type = ModelType.new(starting_km: nil)
+        model_type.valid?
+        expect(model_type.errors.include?(:starting_km)).to be true
+      end
+
+      it 'a distância inicial deve ser maior do que zero' do
+        model_type = ModelType.new(starting_km: 0)
+        model_type.valid?
+        expect(model_type.errors.include?(:starting_km)).to be true
+      end
+
+      it 'a distância inicial deve ser um número inteiro' do
+        model_type = ModelType.new(starting_km: 1.5)
+        model_type.valid?
+        expect(model_type.errors.include?(:starting_km)).to be true
+      end
+
+      it 'a distância final deve ser obrigatório' do
+        model_type = ModelType.new(final_km: nil)
+        model_type.valid?
+        expect(model_type.errors.include?(:final_km)).to be true
+      end
+
+      it 'a distância final deve ser maior do que zero' do
+        model_type = ModelType.new(final_km: 0)
+        model_type.valid?
+        expect(model_type.errors.include?(:final_km)).to be true
+      end
+
+      it 'a distância final deve ser um número inteiro' do
+        model_type = ModelType.new(final_km: 1.5)
+        model_type.valid?
+        expect(model_type.errors.include?(:final_km)).to be true
+      end
+
+      it 'o tempo de entrega deve ser obrigaatório' do
+        model_type = ModelType.new(deadline: nil)
+        model_type.valid?
+        expect(model_type.errors.include?(:deadline)).to be true
+      end
+
+      it 'o tempo de entrega deve ser maior do que zero' do
+        model_type = ModelType.new(deadline: 0)
+        model_type.valid?
+        expect(model_type.errors.include?(:deadline)).to be true
+      end
+
+      it 'o tempo de entrega deve ser um número inteiro' do
+        model_type = ModelType.new(deadline: 1.5)
+        model_type.valid?
+        expect(model_type.errors.include?(:deadline)).to be true
+      end
+    end
+  end
 end
